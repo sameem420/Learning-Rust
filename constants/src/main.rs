3a167f98439d5426254cd50e06273d4aeaaee573
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-13 17:37:48 +0500

    constant program added

---
+++ constants/src/main.rs
@@ -0,0 +1,8 @@
+// always should keep upper-case name and constant has fixed value
+const PI:f32 = 3.1416; 
+const FAV_NUMBER:u8 = 7; 
+
+fn main() {
+    println!("The value of PI is : {}", PI);
+    println!("My Favorite number is : {}", FAV_NUMBER);
+}
