fe0bef3199048ce7813cf05c03a4850b9a3f80a9
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-05-16 17:22:26 +0500

    added the shadowing program

---
+++ shadowing/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+   let x = 7;
+   println!("My favorite number is : {}", x);
+    // declaring the same variable with different value is called shadowing
+   let x = "Sameem";
+   println!("My name is : {}", x);
+}
